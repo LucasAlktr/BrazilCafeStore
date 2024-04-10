dcc0b8e7f27495fbd40f223ad870367db84b1b27
LucasAlktr <lucasbenington@hotmail.com>
2024-04-09 22:44:49 -0400

    page not found adjustment

--- br_cafe_store_app/src/pages/PageNotFound/index.js
+++ br_cafe_store_app/src/pages/PageNotFound/index.js
@@ -1,15 +1,20 @@
 import React from "react";
-import './style.scss'
+import "./style.scss";
 
-const PageNotFound = () => { 
-    return(
+const PageNotFound = () => {
+  return (
     <>
-        <div className="container-error-page">
-            <h1 className="title-error-page">Page Not Found</h1>
-            <p className="text-error-page"><b>Oh no! It seems that this page does not exist. Please verify the address.</b></p>
-        </div>
+      <div className="container-error-page">
+        <h1 className="title-error-page">Page Not Found</h1>
+        <p className="text-error-page">
+          <b>
+            Oh no! It seems that this page does not exist. <br />
+            Please verify the address.
+          </b>
+        </p>
+      </div>
     </>
-    );
-}
+  );
+};
 
 export default PageNotFound;
